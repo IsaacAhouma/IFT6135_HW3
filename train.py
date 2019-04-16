8ad04c4bc8799fb63e5b3da9459a90e3e72e9b2f
IsaacAhouma <ivorianboy@gmail.com>
2019-04-16 12:33:45 -0400

    fix train procedure

--- train.py
+++ train.py
@@ -66,20 +66,48 @@ def get_data_loader(dataset_location, batch_size):
     def lines_to_np_array(lines):
         return np.array([[int(i) for i in line.split()] for line in lines])
     splitdata = []
-    for splitname in ["train", "valid", "test"]:
-        filename = "binarized_mnist_%s.amat" % splitname
-        filepath = os.path.join(dataset_location, filename)
-        utils.download_url(URL + filename, dataset_location)
-        with open(filepath) as f:
-            lines = f.readlines()
-        #f.close()
-        x = lines_to_np_array(lines).astype('float32')
-        x = x.reshape(x.shape[0], 1, 28, 28)
-        y = np.zeros((x.shape[0], 1)).astype('float32')
-        # pytorch data loader
-        dataset = data_utils.TensorDataset(torch.from_numpy(x), torch.from_numpy(y))
-        dataset_loader = data_utils.DataLoader(dataset, batch_size=batch_size, shuffle=splitname == "train")
-        splitdata.append(dataset_loader)
+    splitname = "train"
+    filename = "binarized_mnist_%s.amat" % splitname
+    filepath = os.path.join(dataset_location, filename)
+    utils.download_url(URL + filename, dataset_location)
+    with open(filepath) as f:
+        lines = f.readlines()
+    # f.close()
+    x = lines_to_np_array(lines).astype('float32')
+    x = x.reshape(x.shape[0], 1, 28, 28)
+    y = np.zeros((x.shape[0], 1)).astype('float32')
+    # pytorch data loader
+    dataset = data_utils.TensorDataset(torch.from_numpy(x), torch.from_numpy(y))
+    dataset_loader = data_utils.DataLoader(dataset, batch_size=batch_size, shuffle=splitname == "train")
+    splitdata.append(dataset_loader)
+    splitname = "valid"
+    filename = "binarized_mnist_%s.amat" % splitname
+    filepath = os.path.join(dataset_location, filename)
+    utils.download_url(URL + filename, dataset_location)
+    with open(filepath) as f:
+        lines = f.readlines()
+    # f.close()
+    x = lines_to_np_array(lines).astype('float32')
+    x = x.reshape(x.shape[0], 1, 28, 28)
+    y = np.zeros((x.shape[0], 1)).astype('float32')
+    # pytorch data loader
+    dataset = data_utils.TensorDataset(torch.from_numpy(x), torch.from_numpy(y))
+    dataset_loader = data_utils.DataLoader(dataset, batch_size=batch_size, shuffle=splitname == "train")
+    splitdata.append(dataset_loader)
+    splitname = "test"
+    filename = "binarized_mnist_%s.amat" % splitname
+    filepath = os.path.join(dataset_location, filename)
+    utils.download_url(URL + filename, dataset_location)
+    with open(filepath) as f:
+        lines = f.readlines()
+    # f.close()
+    x = lines_to_np_array(lines).astype('float32')
+    x = x.reshape(x.shape[0], 1, 28, 28)
+    y = np.zeros((x.shape[0], 1)).astype('float32')
+    # pytorch data loader
+    dataset = data_utils.TensorDataset(torch.from_numpy(x), torch.from_numpy(y))
+    dataset_loader = data_utils.DataLoader(dataset, batch_size=batch_size, shuffle=splitname == "train")
+    splitdata.append(dataset_loader)
     return splitdata
 
 
